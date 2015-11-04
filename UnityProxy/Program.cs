52442fc01130c75d72c663649a6818ee0240688a
Srdja Stetic-Kozic <srdjas@nordeus.eu>
2015-11-04 13:42:11 +0100

    Initial commit

---
+++ UnityProxy/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace UnityProxy
+{
+	class Program
+	{
+		/// <summary>
+		/// Magic string our build pipeline writes to the log after a successful build.
+		/// </summary>
+		private const string SuccessMagicString = "Successful build ~0xDEADBEEF";
+
+		static void Main(string[] args)
+		{
+			string unityPath, artifactsPath;
+			int startingArgumentIndex = ParseArguments(args, out unityPath, out artifactsPath);
+			string logPath = Path.GetTempFileName();
+
+			Watcher watcher = new Watcher(logPath);
+			Thread watcherThread = new Thread(watcher.Run);
+			watcherThread.Start();
+
+			Process unity = new Process();
+			unity.StartInfo = new ProcessStartInfo(unityPath);
+
+			unity.StartInfo.Arguments = "-logFile \"" + logPath + "\"";
+
+			for (int i = startingArgumentIndex; i < args.Length; i++)
+			{
+				unity.StartInfo.Arguments += " \"" + args[i] + "\"";
+			}
+
+			Console.WriteLine("Starting Unity with arguments: " + unity.StartInfo.Arguments);
+
+			unity.Start();
+
+			Console.WriteLine("##teamcity[setParameter name='{0}' value='{1}']", "unityPID", unity.Id);
+
+			unity.WaitForExit();
+			watcher.Stop();
+			watcherThread.Join();
+
+			if (artifactsPath != null)
+			{
+				SaveArtifacts(artifactsPath, watcher.FullLog);
+			}
+
+			if (watcher.FullLog.Contains(SuccessMagicString))
+			{
+				Console.WriteLine("Success.");
+				Environment.Exit(0);
+			}
+			else
+			{
+				Console.WriteLine("Failure.");
+				Environment.Exit(1);
+			}
+		}
+
+		/// <summary>
+		/// Saves build artifacts (log file) to the specified path.
+		/// </summary>
+		private static void SaveArtifacts(string artifactsPath, string logText)
+		{
+			Directory.CreateDirectory(artifactsPath);
+
+			File.WriteAllText(artifactsPath + "/editor.log", logText);
+		}
+
+		/// <summary>
+		/// Parses command line arguments.
+		/// </summary>
+		/// <param name="args">Arguments</param>
+		/// <param name="unityPath">Path to the unity executable.</param>
+		/// <param name="artifactsPath">PAth where the artifacts should be saved.</param>
+		/// <returns>The number of arguments parsed.</returns>
+		private static int ParseArguments(string[] args, out string unityPath, out string artifactsPath)
+		{
+			unityPath = args[0];
+			artifactsPath = null;
+
+			if (args.Length > 1)
+			{
+				if (args[1] == "-artifactsPath")
+				{
+					artifactsPath = args[2];
+					return 3;
+				}
+			}
+			return 1;
+		}
+	}
+}
---
+++ UnityProxy/Watcher.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+
+namespace UnityProxy
+{
+	/// <summary>
+	/// Watches the Unity log file and redirects it to standard output.
+	/// </summary>
+	public class Watcher
+	{
+		/// <summary>
+		/// Magic string for detecting progress bar messages.
+		/// </summary>
+		private const string ProgressBarMarker = "DisplayProgressbar: ";
+
+		/// <summary>
+		/// Size of the log when it was previously read.
+		/// </summary>
+		private static long previousLogSize = 0;
+
+		/// <summary>
+		/// Indicates if this thread should stop.
+		/// </summary>
+		private volatile bool shouldStop = false;
+
+		/// <summary>
+		/// Full log text.
+		/// </summary>
+		private volatile string fullLog = "";
+
+		/// <summary>
+		/// Path to the log file.
+		/// </summary>
+		private string logPath;
+
+
+		/// <summary>
+		/// Gets the full log text.
+		/// </summary>
+		public string FullLog
+		{
+			get
+			{
+				return fullLog;
+			}
+		}
+
+		/// <summary>
+		/// Creates a new Watcher that will read the log from the specified path.
+		/// </summary>
+		/// <param name="logPath"></param>
+		public Watcher(string logPath)
+		{
+			this.logPath = logPath;
+		}
+
+		public void Run()
+		{
+			while (true)
+			{
+				if (File.Exists(logPath))
+				{
+					using (FileStream stream = new FileStream(logPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+					{
+						stream.Position = previousLogSize;
+						previousLogSize = stream.Length;
+
+						using (StreamReader reader = new StreamReader(stream))
+						{
+							string newText = reader.ReadToEnd();
+							LogProgressMessages(newText);
+							fullLog += newText;
+							Console.Write(newText);
+						}
+					}
+				}
+
+				if (shouldStop) break;
+				
+				System.Threading.Thread.Sleep(1000);
+			}
+		}
+
+		/// <summary>
+		/// Searches for progress bar messages and forwards them to TeamCity.
+		/// </summary>
+		/// <param name="text"></param>
+		private void LogProgressMessages(string text)
+		{
+			string[] lines = text.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+			for (int i = 0; i < lines.Length; i++)
+			{
+				if (lines[i].StartsWith(ProgressBarMarker))
+				{
+					string progressName = lines[i].Substring(ProgressBarMarker.Length);
+					Console.WriteLine();
+					Console.WriteLine("##teamcity[progressMessage '" + progressName + "']");
+				}
+			}
+		}
+
+		//Stops the watcher.
+		public void Stop()
+		{
+			shouldStop = true;
+		}
+	}
+}
